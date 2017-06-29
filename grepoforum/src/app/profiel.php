894f8ea581e983664d4d0dd93a873543c918c411
Erwinoostland <erwinoostland1@hotmail.com>
2017-06-29 19:43:02 +0200

    Add files via upload

---
+++ grepoforum/src/app/profiel.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include_once('authentication.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Grepo Forum</title>
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css"
+</head>
+
+<?php
+include 'footer.php';
+?>
+<body>
+<img src="../img/banner1.jpg" width="100%">
+<nav class="navbar">
+    <ul>
+        <li class="nav"><a href="index.php">Terug</a></li>
+    </ul>
+</nav>
+</body>
+</html>
